38599136af2e6a9c190e4472bd4082241b27c5b3
ribbery009 <dudas.peter.ovb@gmail.com>
2021-12-30 15:25:40 +0100

    addedJWTtokenHandle

--- index.js
+++ index.js
@@ -10,46 +10,90 @@ const bodyParser = require("body-parser");
 const dbConnect = require("./db");
 const jsonwebtoken = require("jsonwebtoken");
 const { request } = require("http");
-
+const auth = require("./auth");
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 dbConnect();
 
-app.post("/login", async (req, res) => {
-    console.log(req.body)
-    if (!req.body.username || !req.body.password) {
-        res.json({ result: "error", message: "Invalid password or username" });
-        return;
-    }
-    let doc = await Users.findOne({ username: req.body.username });
-    if (doc) {
-        if (bcrypt.compareSync(req.body.password, doc.password)) {
-            const payload = {
-                id: doc._id,
-                level: doc.level,
-                username: doc.username
-            };
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+  });
 
-            let token = jwt.sign(payload);
-            console.log(token);
-            res.json({ result: "success", token, message: "Login successfully" });
-        } else {
-            // Invalid password
-            res.json({ result: "error", message: "Invalid password" });
-        }
-    } else {
-        // Invalid username
-        res.json({ result: "error", message: "Invalid username" });
-    }
+  
+app.post("/login", async (req, res) => {
+const test = User.findOne({ email: req.body.email })
+
+    User.findOne({ email: req.body.email })
+
+        // if email exists
+        .then((user) => {
+            // compare the password entered and the hashed password found
+            bcrypt
+                .compare(req.body.password, user.password)
+
+                // if the passwords match
+                .then((passwordCheck) => {
+                   
+                    // check if password matches
+                    if (!passwordCheck) {
+                        return res.status(400).send({
+                            message: "Passwords does not match",
+                            error,
+                        });
+                    }
+
+                    //   create JWT token
+                    console.log(user)
+                    const token = jsonwebtoken.sign(
+                        {
+                            id: user._id,
+                            email: user.email,
+                        },
+                        "RANDOM-TOKEN",
+                        { expiresIn: "24h" }
+                    );
+
+                    //   return success response
+                    res.status(200).send({
+                        message: "Login Successful",
+                        email: user.email,
+                        token,
+                    });
+                })
+                // catch error if password do not match
+                .catch((error) => {
+                    res.status(400).send({
+                        message: "Passwords does not match",
+                        error,
+                    });
+                });
+        })
+        // catch error if email does not exist
+        .catch((e) => {
+            res.status(404).send({
+                message: "Email not found",
+                e,
+            });
+        });
+
 });
 
 app.post("/register", async (req, res) => {
-   
 
 
-console.log(req.body)
+
+    console.log(req.body)
     try {
         req.body.password = await bcrypt.hash(req.body.password, 8);
 
@@ -60,16 +104,16 @@ console.log(req.body)
         //   { expiresIn: "365d" }
         // );
 
-  
+
         // req.body.activated_token = token;
         let user = new User({
             email: req.body.email,
-            username:req.body.username,
-            password:req.body.password
+            username: req.body.username,
+            password: req.body.password
         });
 
         user
-        .save()
+            .save()
         // return success if the new user is added to the database successfully
 
 
@@ -80,7 +124,7 @@ console.log(req.body)
         //     // perform actions on the collection object
 
         //     const users = await collection.insertOne(user);
-          
+
         //     client.close();
         // });
 
@@ -91,4 +135,10 @@ console.log(req.body)
         res.send({ error: "insert error" });
     }
 });
+
+app.get("/auth-endpoint", auth, (request, response) => {
+    response.send({ message: "You are authorized to access me" });
+  });
+
+
 app.listen(8080)
